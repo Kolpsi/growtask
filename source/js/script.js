8bb38c2c7e3886e361501cdb4b4d39219e0568e4
Kolpsi <tykataf@gmail.com>
2022-02-15 20:41:43 +0500

    реализовал два слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -25,3 +25,38 @@ modalClose.addEventListener('click', function(evt) {
   evt.preventDefault();
   modal.style.display = 'none';
 })
+
+$(document).ready(function(){
+  $(".slide-one").owlCarousel({
+    loop: true,
+      nav:true,
+      responsive: {
+       0:{
+            items:1,
+            navText: ["", "<img src='../img/button-next.svg'>"]
+        },
+        1200:{
+            items:3,
+            nav:true,
+            navText: ["<img src='../img/button-next.svg'>", "<img src='../img/button-next.svg'>"]
+        }
+      }
+    });
+    $(".slide-two").owlCarousel({
+      loop: true,
+        nav:true,
+        responsive: {
+         0:{
+              items:1,
+              navText: ["<img src='../img/card-back.svg'>", "<img src='../img/card-next.svg'>"],
+              stagePadding: 50,
+              margin: 20
+          },
+          1200:{
+              items:3,
+              nav:true,
+              navText: ["<img src='../img/button-next.svg'>", "<img src='../img/button-next.svg'>"]
+          }
+        }
+      });
+});
