f13db3ee0b26ef3f52f8257d9715de100bc4d7c2
Kolpsi <tykataf@gmail.com>
2022-02-16 22:26:39 +0500

    Подготовка к шаблонизации

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ var postcss = require("gulp-postcss");
 var csso = require('gulp-csso');
 var server = require("browser-sync").create();
 var rename = require("gulp-rename");
+var del =  require("del");
+var jsmin = require('gulp-jsmin');
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var imagemin = require("gulp-imagemin");
 
 gulp.task("css", function() {
 	return gulp.src("source/less/style.less")
@@ -19,9 +24,41 @@ gulp.task("css", function() {
     	.pipe(csso())
     	.pipe(rename("style-min.css"))
     	.pipe(sourcemap.write("."))
-    	.pipe(gulp.dest("source/css"))
+    	.pipe(gulp.dest("build/css"))
     	.pipe(server.stream());
 });
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/*.ico",
+    "source/css/owl.carousel.min.css"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("js", function () {
+  return gulp.src('source/js/*.js')
+    .pipe(jsmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+
 
 gulp.task("server", function () {
   server.init({
@@ -35,6 +72,25 @@ gulp.task("server", function () {
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "js",
+  "html",
+  "images"
+));
 
 gulp.task("start", gulp.series("css", "server"));
 
--- source/js/owl.js
+++ source/js/owl.js
@@ -1,38 +1,38 @@
 /**
  * @description модуль активации и настройки карусели
  */
-$(document).ready(function(){
-    $(".slide-one").owlCarousel({
-      loop: true,
-        nav:true,
-        responsive: {
-         0:{
-              items:1,
-              navText: ["", "<img class='nav-next'>"]
-          },
-          1200:{
-            items:1,
-            navText: ["<img class='nav-prev'>", "<img class='nav-next'>"],
-            stagePadding: 480,
-            margin: 0
-          }
-        }
-      });
-      $(".slide-two").owlCarousel({
-        loop: true,
-          nav:true,
-          responsive: {
-           0:{
-                items:1,
-                navText: ["<img class='nav-prev'>", "<img class='nav-next'>"],
-                stagePadding: 50,
-                margin: 20
-            },
-            1200:{
-                items:3,
-                nav:true,
-                navText: ["<img class='nav-prev'>", "<img class='nav-next'>"]
-            }
-          }
-        });
+$(document).ready(function () {
+  $(".slide-one").owlCarousel({
+    loop: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+        navText: ["", "<img  class='nav-next'>"]
+      },
+      1200: {
+        items: 1,
+        navText: ["<img  class='nav-prev'>", "<img  class='nav-next'>"],
+        stagePadding: 480,
+        margin: 0
+      }
+    }
   });
+  $(".slide-two").owlCarousel({
+    loop: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+        navText: ["<img  class='nav-prev'>", "<img  class='nav-next'>"],
+        stagePadding: 50,
+        margin: 20
+      },
+      1200: {
+        items: 3,
+        nav: true,
+        navText: ["<img class='nav-prev'>", "<img  class='nav-next'>"]
+      }
+    }
+  });
+});
